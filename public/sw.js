725b4613fd4d50a4c47b792cfcbb404b92973edf
Shravan <shravan45x@gmail.com>
2026-02-02 20:15:21 +0530

    Fix: All asset paths now use /lets-know/ base for GitHub Pages

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,11 @@
 const CACHE_NAME = 'intel-core-v1';
+const BASE_PATH = '/lets-know/';
 const ASSETS = [
-    './',
-    'index.html',
-    'manifest.json',
-    'android-launchericon-192-192.png',
-    'android-launchericon-512-512.png'
+    BASE_PATH,
+    BASE_PATH + 'index.html',
+    BASE_PATH + 'manifest.json',
+    BASE_PATH + 'android-launchericon-192-192.png',
+    BASE_PATH + 'android-launchericon-512-512.png'
 ];
 
 self.addEventListener('install', (event) => {
